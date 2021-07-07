72e12b3e32c2687bd5ee667cc7ad7f7051a82451
d00telemental <4rterius@gmail.com>
2021-07-07 09:26:19 +0300

    Name dumping - support all three games

--- NameResearch/NameResearch.cpp
+++ NameResearch/NameResearch.cpp
@@ -2,7 +2,7 @@
 #include "../Common.h"
 
 
-SPI_PLUGINSIDE_SUPPORT(L"NameResearch", L"0.1.0", L"d00telemental", SPI_GAME_LE1, SPI_VERSION_ANY);
+SPI_PLUGINSIDE_SUPPORT(L"NameResearch", L"0.1.0", L"d00telemental", SPI_GAME_LE1 | SPI_GAME_LE2 | SPI_GAME_LE3, SPI_VERSION_ANY);
 
 SPI_PLUGINSIDE_POSTLOAD;
 SPI_PLUGINSIDE_ASYNCATTACH;
@@ -91,29 +91,55 @@ SPI_IMPLEMENT_ATTACH
     Common::OpenConsole();
     writeln(L"Attach - hello!");
 
-    auto moduleBase = Common::GetModuleBaseAddress(L"MassEffect1.exe");
-    writeln(L"Attach - module base is %p", moduleBase);
+    SPIGameVersion hostGame;
+    InterfacePtr->GetHostGame(&hostGame);
+    writeln(L"Attach - host game is %d", static_cast<int>(hostGame));
 
-    Names.Chunks = reinterpret_cast<FNameEntry**>(0x16A2090 + moduleBase);
 
-    for (size_t i = 0; i < 2; i++)
+    // Set game-dependent parameters.
+
+    wchar* varModuleName = nullptr;
+    uint64 varChunksOffset = 0;
+
+    switch (hostGame)
     {
-        FNameEntry* nameEntry = Names.Chunks[i];
+    case SPIGameVersion::LE1:
+        varModuleName = L"MassEffect1.exe";
+        varChunksOffset = 0x16A2090;
+        break;
+    case SPIGameVersion::LE2:
+        varModuleName = L"MassEffect2.exe";
+        varChunksOffset = 0x1668A10;
+        break;
+    case SPIGameVersion::LE3:
+        varModuleName = L"MassEffect3.exe";
+        varChunksOffset = 0x17B33D0;
+        break;
+    }
+
+
+    auto moduleBase = Common::GetModuleBaseAddress(varModuleName);
+    auto chunksBase = moduleBase + varChunksOffset;
+    writeln(L"Attach - module base is %p, chunks base is %p", moduleBase, chunksBase);
+
+    // Set the global pool address.
+
+    Names.Chunks = reinterpret_cast<FNameEntry**>(chunksBase);
+
+    for (int poolCounter = 0; poolCounter < 2; poolCounter++)
+    {
+        FNameEntry* nameEntry = Names.Chunks[poolCounter];
         int nameCounter = 0;
-        do
+        while (nameEntry->Index.Length != 0)
         {
-            if (nameEntry->Index.Length == 0)
-            {
-                break;
-            }
+            //writeln(L"Attach - nameEntry[%d][%d]: offset = %d, len = %d, ? = %d; ?? = %p; text = %S",
+            //    poolCounter, nameCounter++,
+            //    nameEntry->Index.Offset, nameEntry->Index.Length, nameEntry->Index.Chunk,
+            //    nameEntry->HashNext, *&(nameEntry->AnsiName));
 
-            writeln(L"Attach - nameEntry[%llu][%d]: offset = %d, len = %d, ? = %d; ?? = %p; text = %S",
-                i, nameCounter++,
-                nameEntry->Index.Offset, nameEntry->Index.Length, nameEntry->Index.Chunk,
-                nameEntry->HashNext, *&(nameEntry->AnsiName));
-
+            writeln(L"NameDump - [%d][%d] = %S", poolCounter, nameCounter++, nameEntry->AnsiName);
             nameEntry = (FNameEntry*)((byte*)nameEntry + 12 + nameEntry->Index.Length + 1);
-        } while (true);
+        };
     }
 
     return true;
