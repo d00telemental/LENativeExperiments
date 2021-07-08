58e0c93d1236b8bc66fa1e2b96203ced547c4a05
d00telemental <4rterius@gmail.com>
2021-07-08 16:40:23 +0300

    Add initial version of LE1DebugModeUncrasher

---
+++ LE1DebugCamUncrasher/LE1DebugModeUncrasher.cpp
@@ -0,0 +1,106 @@
+#include "../Interface.h"
+#include "../Common.h"
+
+
+SPI_PLUGINSIDE_SUPPORT(L"LE1DebugModeUncrasher", L"0.1.0", L"d00telemental", SPI_GAME_LE1, SPI_VERSION_ANY);
+SPI_PLUGINSIDE_POSTLOAD;
+SPI_PLUGINSIDE_ASYNCATTACH;
+
+
+// This is the function which crashes after 'toggledebugcamera':
+// ================================================================
+
+// DWORD UGFxMovie::SomeMethod()
+// {
+//     int localPlayerIndex = this->LocalPlayerOwnerIndex;
+//     if (localPlayerIndex < 0)
+//     {
+//         localPlayerIndex = 0;
+//     }
+//     APlayerController* localController = nullptr;
+//     UGameEngine* engine = Cast<UGameEngine*>(GEngine);
+//     if (localPlayerIndex < engine->GamePlayers.Count)
+//     {
+//         ULocalPlayer* localPlayer = engine->GamePlayers(localPlayerIndex);
+//         if (localPlayer)
+//         {
+//             localController = localPlayer->Actor;
+//         }
+//     }
+//     UPlayerInput* playerInput = localController->PlayerInput;
+//     if (!playerInput->bUsingGamepad)
+//     {
+//         return 0;
+//     }
+//     return SomeControllerInputMethod();  // don't care to reverse engineer this
+// }
+
+// ================================================================
+// The code below patches out the last 6 lines out,
+// which means that controller doesn't work,
+// but at least the game doesn't crash outright.
+
+
+// --------------------------------------------------
+SPI_IMPLEMENT_ATTACH
+{
+    Common::OpenConsole();
+    writeln(L"Attach - hello!");
+
+    byte* crashingMethod;
+    char* crashingPattern = "48 83 EC 28 8B 81 88 01 00 00 85 C0 79 0C C7 81 88 01 00 00 00 00 00 00 33 C0";
+    
+    auto rc = InterfacePtr->FindPattern((void**)&crashingMethod, crashingPattern);
+    if (rc != SPIReturn::Success)
+    {
+        writeln(L"Attach - failed to find the pattern: %d / %s", rc, SPIReturnToString(rc));
+        return false;
+    }
+
+    char replacementBytes[] = "\x90\x90\x90\x90\x90\x90\x90\xEB";
+    uint64 replacementOffset = 0x4B;
+
+    writeln(L"Attach - found pattern at %p, destination is %p, sizeof replacement is 0x%02X !",
+        crashingMethod, crashingMethod + replacementOffset, sizeof replacementBytes - 1);
+
+    // This will use RAII to automatically restore the memory protection.
+    struct RAIIMemoryPatchingContext
+    {
+        byte* Target;
+        char* Replacement;
+        size_t Length;
+        int32 OldProtect;
+        bool NeedsReverse;
+
+        RAIIMemoryPatchingContext(byte* target, char* replacement, size_t length)
+            : Target{ target }, Replacement{ replacement }, Length{ length }, NeedsReverse{ false }
+        {
+            if (!VirtualProtect(target, length, PAGE_EXECUTE_READWRITE, (DWORD*)&OldProtect))
+            {
+                writeln(L"Attach - failed to unprotect the memory, last error = %d", GetLastError());
+                return;
+            }
+
+            memcpy(target, replacement, length);
+            writeln(L"Attach - UGFxMovie::SomeMethod() patched!");
+            NeedsReverse = true;
+        }
+
+        ~RAIIMemoryPatchingContext()
+        {
+            if (NeedsReverse)
+            {
+                VirtualProtect(Target, Length, OldProtect, (DWORD*)&OldProtect);
+            }
+        }
+    } patchingContext{ crashingMethod + replacementOffset, replacementBytes, sizeof replacementBytes - 1 };
+
+    return true;
+}
+
+// --------------------------------------------------
+SPI_IMPLEMENT_DETACH
+{
+    Common::CloseConsole();
+    return true;
+}
