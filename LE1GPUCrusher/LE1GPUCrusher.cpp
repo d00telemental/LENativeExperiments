38956a87fed4f577098e89289fc8e3fd652dbce1
d00telemental <4rterius@gmail.com>
2021-09-25 02:14:42 +0300

    Remove redundant code in GPUCrusher which actually doesn't do much crushing

--- LE1GPUCrusher/LE1GPUCrusher.cpp
+++ LE1GPUCrusher/LE1GPUCrusher.cpp
@@ -1,13 +1,11 @@
 #include "../Interface.h"
 #include "../Common.h"
 
-
 SPI_PLUGINSIDE_SUPPORT(L"LE1GPUCrusher", L"0.1.0", L"d00telemental", SPI_GAME_LE1, SPI_VERSION_ANY);
 SPI_PLUGINSIDE_POSTLOAD;
 SPI_PLUGINSIDE_ASYNCATTACH;
 
 #define writeln(frmt, ...)   fwprintf_s(stdout, L"LE1GPUCrusher - " frmt "\n", __VA_ARGS__);
-
 
 #define REFLECTIONUPDATEREQUIRED_PATTERN "48 89 5C 24 10 48 89 6C 24 18 48 89 74 24 20 57 41 54 41 55 41 56 41 57 48 83 EC 40 48 8B 01 48 8B F2 4C 8B F9 FF 50 48"
 using tReflectionUpdateRequired = unsigned long (*)(void*, void*);
@@ -15,18 +13,14 @@ tReflectionUpdateRequired ReflectionUpdateRequired = nullptr;
 tReflectionUpdateRequired ReflectionUpdateRequired_orig = nullptr;
 unsigned long ReflectionUpdateRequired_hook(void* a, void* b)
 {
-    const auto value = ReflectionUpdateRequired_orig(a, b);
-
     // !!! DANGER !!!
     return TRUE;
-    // !!! DANGER !!!
 
-    writeln(L"ReflectionUpdateRequired_orig => %d", value);
-    return value;
+    //const auto value = ReflectionUpdateRequired_orig(a, b);
+    //writeln(L"ReflectionUpdateRequired_orig => %d", value);
+    //return value;
 }
 
-
-// --------------------------------------------------
 SPI_IMPLEMENT_ATTACH
 {
     Common::OpenConsole();
@@ -48,7 +42,6 @@ SPI_IMPLEMENT_ATTACH
     }
 
     Sleep(15 * 1000);
-
     MessageBoxW(nullptr, L"DO NOT USE IF YOU AREN'T WILLING TO REPLACE YOUR GPU", L"DANGER", MB_OK | MB_ICONERROR);
 
     return true;
